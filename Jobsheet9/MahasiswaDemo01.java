63bf7968cd478d191459800af2a6dcfd8167f6e3
praditadf <achmadpradita17@gmail.com>
2026-04-21 09:08:12 +0700

    Modifikasi Percobaan 1

--- Jobsheet9/MahasiswaDemo01.java
+++ Jobsheet9/MahasiswaDemo01.java
@@ -12,7 +12,9 @@ public class MahasiswaDemo01 {
             System.out.println("1. Mengumpulkan Tugas");
             System.out.println("2. Menilai Tugas");
             System.out.println("3. Melihat Tugas Teratas");
-            System.out.println("4. Melihat Daftar Tugas");
+            System.out.println("4. Melihat Tugas Terbawah");
+            System.out.println("5. Melihat Daftar Tugas");
+            System.out.println("6. Total Tugas Dikumpulkan");
             System.out.print("Pilih: ");
             pilih = scan.nextInt();
             scan.nextLine();
@@ -45,14 +47,24 @@ public class MahasiswaDemo01 {
                     }
                     break;
                 case 4:
+                    Mahasiswa01 lihatBawah = stack.tugasBawah();
+                    if (lihatBawah != null) {
+                        System.out.println("Tugas pertama dikumpulkan oleh " + lihatBawah.nama);
+                    }
+                    break;
+                case 5:
                     System.out.println("Daftar semua tugas");
                     System.out.println("Nama\tNIM\tKelas");
                     stack.print();
+                    break;
+                case 6:
+                    int totalTugas = stack.hitungTugas() +1 ;
+                    System.out.println("Total tugas yang sudah terkumpul saat ini " +totalTugas);
                     break;
                 default:
                     System.out.println("Pilihan tidak valid.");
                     break;
             }
-        } while (pilih >= 1 && pilih <= 4);
+        } while (pilih >= 1 && pilih <= 6);
     }
 }
--- Jobsheet9/StackTugasMahasiswa01.java
+++ Jobsheet9/StackTugasMahasiswa01.java
@@ -56,8 +56,26 @@ public class StackTugasMahasiswa01 {
         }
     }
 
+    Mahasiswa01 tugasBawah() {
+        if (!isEmpty()) {
+            return stack[0];
+        } else {
+            System.out.println("Stack kosong! Tidak ada tugas yang dikumpulkan.");
+            return null;
+        }
+    }
+
+    int hitungTugas() {
+        if (!isEmpty()) {
+            return top;
+        } else {
+            System.out.println("Stack kosong! Tidak ada tugas yang dikumpulkan.");
+            return 0;
+        }
+    }
+
     void print() {
-        for (int i = 0; i <= top; i++) {
+        for (int i = top; i >= 0; i--) {
             System.out.println(stack[i].nama + "\t" + stack[i].nim + "\t" + stack[i].kelas);
         }
         System.out.println("");
